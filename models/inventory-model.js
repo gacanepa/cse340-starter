9f11ac81a172e0978f3a4b394107de24e0bdc5c0
Gabriel Cánepa <gacanepa@gmail.com>
2024-10-05 12:54:12 -0300

    Fix typo in table name

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,7 +1,7 @@
 const pool = require("../database/");
 
 async function getClassifications() {
-  return await pool.query("SELECT * FROM classifications ORDER BY classification_name");
+  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
 }
 
 module.exports = {
