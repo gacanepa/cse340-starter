220203a25431c65568ee2c2dabff205da7939ed6
Gabriel Cánepa <gacanepa@gmail.com>
2024-10-05 20:41:46 -0300

    Add function to get inventory by id

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,6 +4,11 @@ async function getClassifications() {
   return await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
 }
 
+async function getInventoryById(id) {
+  return await pool.query("SELECT * FROM public.inventory WHERE id = $1", [id]);
+}
+
 module.exports = {
   getClassifications,
+  getInventoryById,
 };
