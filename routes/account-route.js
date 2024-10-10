71953dec052528933126c8541963613441297706
Gabriel Cánepa <gacanepa@gmail.com>
2024-10-10 19:38:59 -0300

    Update registration

--- routes/account-route.js
+++ routes/account-route.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/account-controller");
 const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation');
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegistration));
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
